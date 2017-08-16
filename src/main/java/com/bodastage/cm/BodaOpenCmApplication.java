44a8854e18405585c2e9060575078380918566c4
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2017-08-16 11:36:54 +0300

    Enable cross origin requests

--- src/main/java/com/bodastage/cm/BodaOpenCmApplication.java
+++ src/main/java/com/bodastage/cm/BodaOpenCmApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 //@EnableJpaRepositories(repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class, basePackages = "com.bodastage.cm")
@@ -21,4 +25,20 @@ public class BodaOpenCmApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BodaOpenCmApplication.class, args);
 	}
+	
+	/**
+	 * Configure CORS
+	 * 
+	 * @return
+	 */
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+                //registry.addMapping("/api").allowedOrigins("http://localhost:7000");
+            }
+        };
+    }
 }
